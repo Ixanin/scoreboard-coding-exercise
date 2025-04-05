d172fd71b15774cdaa4b2af3174fb12ac19571fb
Krzysiek <k.trekiel@poczta.pl>
2025-04-05 15:43:22 +0200

    feat(tooling): fix lint issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
 
 test('renders Vite + React heading', () => {
-    render(<App />)
-    const headingElement = screen.getByText(/Vite \+ React/i)
-    expect(headingElement).toBeInTheDocument()
-})
+  render(<App />);
+  const headingElement = screen.getByText(/Vite \+ React/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
 test('renders count button', () => {
-    render(<App />)
-    const buttonElement = screen.getByRole('button', { name: /count is/i })
-    expect(buttonElement).toBeInTheDocument()
-})
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: /count is/i });
+  expect(buttonElement).toBeInTheDocument();
+});

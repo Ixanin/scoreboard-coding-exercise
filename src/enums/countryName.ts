920bc06c89a0d1bb903b15aec84bf447339b52f7
Krzysiek <k.trekiel@poczta.pl>
2025-04-06 13:17:52 +0200

    refactor(MatchesTable): remove redundant country name field

---
+++ src/enums/countryName.ts
@@ -0,0 +1,24 @@
+import { CountryCode } from './';
+
+export const CountryName: Record<CountryCode, string> = {
+  [CountryCode.Mexico]: 'Mexico',
+  [CountryCode.Canada]: 'Canada',
+  [CountryCode.Spain]: 'Spain',
+  [CountryCode.Brazil]: 'Brazil',
+  [CountryCode.Germany]: 'Germany',
+  [CountryCode.France]: 'France',
+  [CountryCode.Uruguay]: 'Uruguay',
+  [CountryCode.Italy]: 'Italy',
+  [CountryCode.Argentina]: 'Argentina',
+  [CountryCode.Australia]: 'Australia',
+  [CountryCode.Poland]: 'Poland',
+  [CountryCode.Japan]: 'Japan',
+  [CountryCode.SouthKorea]: 'South Korea',
+  [CountryCode.Netherlands]: 'Netherlands',
+  [CountryCode.Belgium]: 'Belgium',
+  [CountryCode.Portugal]: 'Portugal',
+  [CountryCode.Sweden]: 'Sweden',
+  [CountryCode.Switzerland]: 'Switzerland',
+  [CountryCode.Denmark]: 'Denmark',
+  [CountryCode.Norway]: 'Norway',
+};
